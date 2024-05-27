43ee267f75bb3dd5570888656bc8d8977c5dca4b
guskirb <anguskirby1998@gmail.com>
2024-05-27 18:47:28 +0100

    Rewrite to use express

--- index.js
+++ index.js
@@ -1,40 +1,88 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
 
-const dir = [];
+const options = {
+  root: path.join(__dirname) + '/pages/'
+}
 
-fs.readdir('./pages', (err, files) => {
-  files.forEach(file => {
-    dir.push('./pages/' + file);
+app.get('/', (req, res) => {
+  res.sendFile('index.html', options, (err) => {
+    if (err) {
+      console.error(err);
+    }
   });
-  console.log(dir);
 })
 
-http.createServer((req, res) => {
-  const path = url.parse(req.url, true);
-  const filename = path.path === '/' ? './pages/index.html' : './pages' + path.pathname + '.html';
+app.get('/about', (req, res) => {
+  res.sendFile('about.html', options, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+})
 
-  console.log(filename);
+app.get('/contact-me', (req, res) => {
+  res.sendFile('contact-me.html', options, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+})
 
-  if (dir.includes(filename)) {
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        console.error(err);
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    })
-  } else {
-    fs.readFile('./pages/404.html', (err, data) => {
-      if (err) {
-        console.error(err);
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end();
-    })
+app.get('*', (req, res) => {
+  res.sendFile('404.html', options, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+})
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error('Error');
   }
 
-}).listen(8080);
+  console.log(`Server listening on port ${PORT}`)
+})
+
+// const dir = [];
+
+// fs.readdir('./pages', (err, files) => {
+//   files.forEach(file => {
+//     dir.push('./pages/' + file);
+//   });
+//   console.log(dir);
+// })
+
+// http.createServer((req, res) => {
+//   const path = url.parse(req.url, true);
+//   const filename = path.path === '/' ? './pages/index.html' : './pages' + path.pathname + '.html';
+
+//   console.log(filename);
+
+//   if (dir.includes(filename)) {
+//     fs.readFile(filename, (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.write(data);
+//       return res.end();
+//     })
+//   } else {
+//     fs.readFile('./pages/404.html', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//       res.writeHead(200, { 'Content-Type': 'text/html' });
+//       res.write(data);
+//       return res.end();
+//     })
+//   }
+
+// }).listen(8080);
